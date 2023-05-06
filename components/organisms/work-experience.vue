c9814f2d800806908d504508523f504e8abf60a6
akgw <r.akagawa@buysell-technologies.com>
2023-05-06 16:56:06 +0900

    modify main fix year

--- components/organisms/work-experience.vue
+++ components/organisms/work-experience.vue
@@ -4,8 +4,8 @@
       <v-timeline reverse dense>
         <timeline-item title="2013.04 ~ 2016.03" :bodies="['GREE, inc.', 'グリー株式会社']" />
         <timeline-item title="2016.04 ~ 2017.10" :bodies="['Hokuden Information System Service Company, Inc', '北電情報システムサービス株式会社']" />
-        <timeline-item title="2017.11 ~ 2022.06" :bodies="['SMSC Co., Ltd.', '株式会社エス・エム・エスキャリア']" />
-        <timeline-item title="2022.07 ~ " :bodies="['BuySell Technologies Co., Ltd.', '株式会社BuySell Technologies']" />
+        <timeline-item title="2017.11 ~ 2021.06" :bodies="['SMSC Co., Ltd.', '株式会社エス・エム・エスキャリア']" />
+        <timeline-item title="2021.07 ~ " :bodies="['BuySell Technologies Co., Ltd.', '株式会社BuySell Technologies']" />
       </v-timeline>
     </v-col>
   </v-row>
